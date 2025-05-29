d1896b1d680795876ec27aa555641baee01a8ea4
angCF <ac84760@gmail.com>
2025-05-28 19:57:46 -0500

    Diseño base del sistema

---
+++ src/main/java/models/Pedido.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author USER
+ */
+public class Pedido {
+    private String nombreMedicamento;
+    private Integer tipoMedicamento;
+    private Integer cantidad;
+    private String distribuidor;
+    private String farmacias;
+}
